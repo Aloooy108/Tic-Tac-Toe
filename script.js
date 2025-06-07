9c1406af4f881354fe385bdd92cd81f52bf8badf
aloooy108 <ali.zaki.alomran@gmail.com>
2025-06-07 20:31:07 +0300

    Add checkGameState function

--- script.js
+++ script.js
@@ -1,29 +1,68 @@
-function game(){//function that keeps score or returns it
-    let score=[0,0]
-    function win(player){
+function game() {//function that keeps score or returns it
+    let score = [0, 0]
+    function win(player) {
         score[player]++
     }
-    function getScore(){
+    function getScore() {
         return score
     }
-    return{getScore,win}
+    return { getScore, win }
 }
-function player(id){
-    const getID=()=>id
-    return {getID}
+function player(id) {
+    const getID = () => id
+    return { getID }
 }
-function gameBoard(){
-    let board=[
-        [0,1,0],
-        [0,2,0],
-        [0,0,0]
+function gameBoard() {
+    let board = [
+        ['x', 'x', 'o'],
+        ['o', 'x', 'x'],
+        ['o', 'x', 'o']
     ]
-    const getSpace=(i,j)=>board[i][j]
-    const writeSpace=(id,i,j)=>board[i][j]=id
-    return {getSpace,writeSpace}
+    const getWinner = () => checkGameState(board)
+    const getSpace = (i, j) => board[i][j]
+    const writeSpace = (id, i, j) => board[i][j] = id
+    const getBoard = () => board
+    return { getSpace, writeSpace, getBoard, getWinner }
 }
-const p1=player(0)
-const p2=player(1)
-const g1=game()
-const gb1=gameBoard()
 
+function checkGameState(board) {
+    const winPatterns = [
+      [ [0,0], [0,1], [0,2] ],
+      [ [1,0], [1,1], [1,2] ],
+      [ [2,0], [2,1], [2,2] ],
+      [ [0,0], [1,0], [2,0] ],
+      [ [0,1], [1,1], [2,1] ],
+      [ [0,2], [1,2], [2,2] ],
+      [ [0,0], [1,1], [2,2] ],
+      [ [0,2], [1,1], [2,0] ]
+    ]
+  
+    for (const pattern of winPatterns) {
+      const [a, b, c] = pattern
+      const first = board[a[0]][a[1]]
+      if (first && first === board[b[0]][b[1]] && first === board[c[0]][c[1]]) {
+        for (let i = 0; i < board.length; i++) {
+          for (let j = 0; j < board[i].length; j++) {
+            board[i][j] = null
+          }
+        }
+        return first
+      }
+    }
+  
+    const isFull = board.every(row => row.every(cell => cell))
+    if (isFull) {
+      for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+          board[i][j] = null
+        }
+      }
+      return 'Draw'
+    }
+  
+    return null
+  }
+const p1 = player(0)
+const p2 = player(1)
+const g1 = game()
+const gb1 = gameBoard()
