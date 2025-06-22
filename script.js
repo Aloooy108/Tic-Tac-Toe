4f80590a73fd8f9f0bc56e9f80000bd0499e61b7
aloooy108 <ali.zaki.alomran@gmail.com>
2025-06-22 23:38:14 +0300

    add playercounter add input function add html id to grid position function

--- script.js
+++ script.js
@@ -14,32 +14,42 @@
 // }
 function gameBoard() {
     let board = [
-        ['X', 'X', 'O'],
-        ['O', null, 'X'],
-        ['O', 'X', 'O']
+        [null, null, null],
+        [null, null, null],
+        [null, null, null]
     ]
     let score = [0, 0]
-
+    let playerCounter = 1//to mod later to see who is the current player
     function getScore() {
         return score
     }
+    const getPlayerCounter = () => playerCounter
     const getWinner = () => checkGameState(board)
     const getSpace = (i, j) => board[i][j]
     const writeSpace = (id, i, j) => {
+        if(board[i][j]!=null)return
         board[i][j] = id
         let gameState = checkGameState(board)
-
+        playerCounter++
         if (gameState == 'X') {
             score[0]++
             displayScore()
+            playerCounter = 1
         }
         if (gameState == 'O') {
             score[1]++
             displayScore()
+            playerCounter = 1
         }
+        if (gameState == "Draw") {
+            playerCounter = 1
+            displayBoard()
+        }
+
+
     }
     const getBoard = () => board
-    return { getSpace, writeSpace, getBoard, getWinner, getScore }
+    return { getSpace, writeSpace, getBoard, getWinner, getScore, getPlayerCounter }
 }
 
 function checkGameState(board) {
@@ -60,12 +70,13 @@ function checkGameState(board) {
         if (first && first === board[b[0]][b[1]] && first === board[c[0]][c[1]]) {
             //clears the board after win
             displayBoard()
-            alert(first+' Wins')
+            alert(first + ' Wins!')
             for (let i = 0; i < board.length; i++) {
                 for (let j = 0; j < board[i].length; j++) {
                     board[i][j] = null
                 }
             }
+            displayBoard()
             score[first]++
             return first
         }
@@ -73,21 +84,20 @@ function checkGameState(board) {
     //check for draw and clears the board if true
     const isFull = board.every(row => row.every(cell => cell))
     if (isFull) {
+        alert('Draw!')
         displayBoard()
         for (let i = 0; i < board.length; i++) {
             for (let j = 0; j < board[i].length; j++) {
                 board[i][j] = null
             }
         }
+        
         return 'Draw'
     }
     //return null if game still going
     displayBoard()
     return null
 }
-// const p1 = player(0)
-// const p2 = player(1)
-// const g1 = game()
 const gb1 = gameBoard();
 
 
@@ -97,7 +107,6 @@ function displayScore() {
     score.innerHTML = scoreArray[0] + " - " + scoreArray[1]
 
 }
-
 function displayBoard() {
     let counter = 0
     let grid = document.getElementsByClassName('gridCell')
@@ -109,4 +118,30 @@ function displayBoard() {
         }
     }
 }
+//function to get the position from 1-9 to [i][j]
+function getGridPosition(num) {
+    const row = Math.floor((num - 1) / 3)
+    const col = (num - 1) % 3
+    return [row,col]
+}
 displayBoard()
+//function that takes click input and invokes writeSpace
+function gridInput() {
+    let grid = document.getElementsByClassName('gridCell')
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].addEventListener('click', () => {
+            let player
+            let playerCounter=gb1.getPlayerCounter()
+            if (playerCounter % 2 == 1) {
+                player = "X"
+            }
+            if (playerCounter % 2 != 1) {
+                player = "O"
+            }
+            let position = getGridPosition(grid[i].getAttribute('id'))
+            gb1.writeSpace(player,position[0], position[1])
+            
+        })
+    }
+}
+gridInput()
